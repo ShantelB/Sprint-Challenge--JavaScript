14669f987fbc42e8b42d16b1d51093b0f6d58140
Shantel Brown <Shantel.Brown2021@gmail.com>
2020-03-27 11:02:00 -0700

    stuff

--- challenges/classes.js
+++ challenges/classes.js
@@ -6,22 +6,47 @@ class CuboidMaker {
     this.width = attributes.width;
     this.height = attributes.height; 
     };
-    volume = function (){
-    return volume = this.length * this.width * this.height;
+    volume () {
+    return  this.length * this.width * this.height;
     };
-    surfaceArea = function() {
-    return surfaceArea = 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+    surfaceArea () {
+    return  2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
     };
-    }
+}
+    // cuboid () {
+    //     
+        
+    // }
+    // }
 
-    class cuboid extends CuboidMaker {
-        constructor(item){
-            super(item);
-            this.length = 4,
-            this.width = 5,
-            this.height = 5,
+    const cuboid = new CuboidMaker(length, width, height)
+    // function cuboid(length, width, height) 
+    {
+        Person.call(length, width, height);
+        length = 4;
+        width = 5;
+        height = 5;
         }
-    }
+
+    // class cuboid extends CuboidMaker {
+    //     constructor(item){
+    //         super(item);
+    //         length = 4;
+    //         width = 5;
+    //         height = 5;
+    //     }
+    // }
+    //         item.length = 4,
+    //         item.width = 5,
+    //         item.height = 5,
+    //     }  
+    //     volume () {
+    //         return volume = this.length * this.width * this.height;
+    //         };
+    //         surfaceArea () {
+    //             return surfaceArea = 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+    //             };
+    // }
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
